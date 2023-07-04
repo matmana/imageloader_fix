adb11b0adfb4f7233c3076acbc04575be36c8792
Mateusz Manart <matmanart@gmail.com>
2023-07-04 13:29:58 +0000

    init changes

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+   name='openpyxl_image_loader_fix',
+   version='1.0',
+   description='A useful module',
+   author='Man Foo',
+   packages=['openpyxl_image_loader_fix'],  #same as name
+)
